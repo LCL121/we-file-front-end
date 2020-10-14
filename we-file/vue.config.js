93b86dee8d462fe8b3f40b0fb63ee3885d054bcb
LCL121 <1391436522@qq.com>
2020-10-15 00:30:37 +0800

    完成了前端代理的配置及部分API的连接测试

--- we-file/vue.config.js
+++ we-file/vue.config.js
@@ -1,7 +1,49 @@
 const path = require('path')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+const qs = require('qs')
 
 module.exports = {
   publicPath: process.env.VUE_APP_BASE_API,
+  devServer: {
+    before: function (app, server, compiler) {
+      app.use(bodyParser.urlencoded({ extended: false }))
+
+      app.all(/^\/api\/v1\/.*/, (req, res) => {
+        console.log(`${req.method} to: http://www.wefile.com:8080${req.url}`)
+
+        if (req.method === 'OPTIONS') {
+          res.sendStatus(200);
+        } else if (req.method === 'GET') {
+          axios.get(`http://www.wefile.com:8080${req.url}`)
+            .then((tempRes) => {
+              const data = tempRes.data
+              res.send(data)
+            })
+            .catch(e => {
+              console.log('get 出错！')
+              const data = e.response.data
+              console.log(data)
+              res.send(data)
+            })
+        } else if (req.method === 'POST') {
+          console.log(req.body)
+          axios.post(`http://www.wefile.com:8080${req.url}`, qs.stringify(req.body))
+            .then(tempRes => {
+              const data = tempRes.data
+              console.log(data)
+              res.send(data)
+            })
+            .catch(e => {
+              console.log('post 出错！')
+              const data = e.response.data
+              console.log(data)
+              res.send(data)
+            })
+        }
+      })
+    }
+  },
   chainWebpack: (config) => {
     config
       .plugin('html')
