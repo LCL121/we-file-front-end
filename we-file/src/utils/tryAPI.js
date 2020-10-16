a87e6e17ea5e3db58ff47a01616c18107f46258a
LCL121 <1391436522@qq.com>
2020-10-16 12:02:02 +0800

    新增API测试

--- we-file/src/utils/tryAPI.js
+++ we-file/src/utils/tryAPI.js
@@ -47,19 +47,10 @@ const tryAPI = async () => {
     .then(res => {
       console.log(res)
       downloadAddress = res.data.address
-      downloadAuthorization = res.data.authorization
+      downloadAuthorization = res.headers.authorization
     })
 
-  // 测试获取上传地址接口
-  // let downloadAddress = ''
-  // let downloadAuthorization = ''
-  // await axios.get(`/api/v1/upload_address/${userId}?file_name=${file1.file_name}&directory=${directory.directory}`)
-  //   .then(res => {
-  //     console.log(res)
-  //     downloadAddress = res.data.address
-  //     downloadAuthorization = res.data.authorization
-  //   })
-
+  // 测试下载功能
   await axios.request({
     url: downloadAddress,
     method: 'GET',
@@ -67,6 +58,46 @@ const tryAPI = async () => {
       authorization: downloadAuthorization
     }
   })
+    .then(res => {
+      console.log(res.data)
+    })
+
+  // 测试上传功能
+  const inputDom = document.createElement('input')
+  inputDom.accept = 'image/png,image/gif,image/jpeg,txt'
+  inputDom.type = 'file'
+  inputDom.onchange = async function (e) {
+    const file = e.target.files[0]
+    const param = new FormData() // 创建form对象
+    param.append('file', file)// 通过append向form对象添加数据
+    console.log(param.get('file')) // FormData私有类对象，访问不到，通过get判断值是否传进去
+
+    // 测试获取上传地址接口
+    let uploadAddress = ''
+    let uploadAuthorization = ''
+    await axios.get(`/api/v1/upload_address/${userId}?file_name=${file.name}&directory=/`)
+      .then(res => {
+        console.log(res)
+        uploadAddress = res.data.address
+        uploadAuthorization = res.headers.authorization
+      })
+
+    const url = `${uploadAddress}/api/v1/upload`
+    console.log(url, uploadAuthorization)
+    axios.request({
+      url,
+      method: 'POST',
+      headers: {
+        authorization: uploadAuthorization,
+        'Content-Type': 'multipart/form-data'
+      },
+      data: param
+    })
+      .then(res => {
+        console.log(res)
+      })
+  }
+  document.body.appendChild(inputDom)
 }
 
 tryAPI()
